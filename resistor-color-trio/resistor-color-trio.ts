b087723f052154acd3d25c1771ef4bb9b88ad8b7
“Lucas <lucaspereira.desenvolvedor@gmail.com>
2023-09-01 17:50:56 -0300

    implementing decodedResistor method

--- resistor-color-trio/resistor-color-trio.ts
+++ resistor-color-trio/resistor-color-trio.ts
@@ -1,3 +1,30 @@
-export function decodedResistorValue() {
-  throw new Error('Remove this statement and implement this function')
+const colorsDictionary:{[color: string]: number} = {
+  'black': 0,
+  'brown': 1,
+  'red': 2,
+  'orange': 3,
+  'yellow': 4,
+  'green': 5,
+  'blue': 6,
+  'violet': 7,
+  'grey': 8,
+  'white': 9
+} 
+
+function calculateResistorValue(colors: string[]): number {
+  return parseInt(colorsDictionary[colors[0]] + '' + colorsDictionary[colors[1]])
+}
+
+export function decodedResistorValue(colors: string[]): string {
+  let resistorValue = calculateResistorValue(colors) * (Math.pow(10, colorsDictionary[colors[2]]))
+
+  if (resistorValue >= 1000000000) {
+    return resistorValue / 1000000000 + " gigaohms"
+  } else if (resistorValue >= 1000000) {
+    return resistorValue / 1000000 + " megaohms"
+  } else if (resistorValue >= 1000){
+    return resistorValue / 1000 + " kiloohms"
+  }
+  
+  return resistorValue + " ohms"
 }
