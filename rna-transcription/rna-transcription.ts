e5a1fdcfe749ad4177233f085415c93c92a53f02
“Lucas <lucaspereira.desenvolvedor@gmail.com>
2023-09-04 18:19:45 -0300

    implementing rna transcription method

--- rna-transcription/rna-transcription.ts
+++ rna-transcription/rna-transcription.ts
@@ -1,3 +1,20 @@
-export function toRna() {
-  throw new Error('Remove this statement and implement this function')
+export function toRna(nucleotideo: string): string {
+  const dnaToRna: { [dnaKey: string]: string } = {
+    G: "C",
+    C: "G",
+    T: "A",
+    A: "U",
+  }
+
+  const invalidInput = "Invalid input DNA."
+
+  return nucleotideo
+  .split("")
+  .map((nucleotideo) => {
+    if (!dnaToRna[nucleotideo]) {
+      throw new Error(invalidInput)
+    }
+    return dnaToRna[nucleotideo]
+  })
+  .join("")
 }
