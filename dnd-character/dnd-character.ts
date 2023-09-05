6a661b829be230ba19bf2df7b65b2063257c592b
“Lucas <lucaspereira.desenvolvedor@gmail.com>
2023-09-05 16:00:10 -0300

    implementing generateabilityScore

--- dnd-character/dnd-character.ts
+++ dnd-character/dnd-character.ts
@@ -1,6 +1,22 @@
 export class DnDCharacter {
+  public strength: number
+  public dexterity: number
+  public constitution: number
+  public intelligence: number
+  public wisdom: number
+  public charisma: number
+  public hitpoints: number
+
+
   public static generateAbilityScore(): number {
-    throw new Error('Remove this statement and implement this function')
+    const values = []
+    for (let i = 0; i < 4; i++) {
+      values.push(Math.floor(Math.random() * 6) + 1)
+    }
+
+    values.sort()
+    values.shift()
+    return values.reduce((valueAccumulator, currentValue) => valueAccumulator + currentValue)
   }
 
   public static getModifierFor(abilityValue: number): number {
