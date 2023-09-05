0f6f3fad4d596ef2186fef1e077c9e0e2a2d4e29
“Lucas <lucaspereira.desenvolvedor@gmail.com>
2023-09-05 00:00:49 -0300

    implementing age method

--- space-age/space-age.ts
+++ space-age/space-age.ts
@@ -1,3 +1,18 @@
-export function age(planet: unknown, seconds: unknown): unknown {
-  throw new Error('Remove this statement and implement this function')
+export function age(planet: string, seconds: number): number {
+  // secondsInEarthYear = days * hours * minutes * seconds
+  const secondsInEarthYear = 365.25 * 24 * 60 * 60
+
+  const planetToEarthYears : { [planet: string]: number } = {
+    earth: secondsInEarthYear,
+    mercury: 0.2408467 * secondsInEarthYear,
+    venus: 0.61519726 * secondsInEarthYear,
+    mars: 1.8808158 * secondsInEarthYear,
+    jupiter: 11.862615 * secondsInEarthYear,
+    saturn: 29.447498 * secondsInEarthYear,
+    uranus: 84.016846 * secondsInEarthYear,
+    neptune: 164.79132 * secondsInEarthYear,
+  }
+
+
+  return parseFloat((seconds / planetToEarthYears[planet]).toFixed(2))
 }
