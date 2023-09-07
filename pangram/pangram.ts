83dac396e0ed51a2dc2559f1fff64359cc6f95e3
“Lucas <lucaspereira.desenvolvedor@gmail.com>
2023-09-07 00:42:25 -0300

    implementing isPangram method

--- pangram/pangram.ts
+++ pangram/pangram.ts
@@ -1,9 +1,10 @@
 export function isPangram(sentense: string): boolean {
-  const alphabet: string = "abcdefghijklmnopqrstuvwxyz"
-  const alphabetArray = alphabet.split("")
+  const alphabetArray: string[] = "abcdefghijklmnopqrstuvwxyz".split("")
   const sentenseArray = sentense.toLowerCase().split("")
+  
   const result = alphabetArray.filter((letter) =>
     sentenseArray.includes(letter)
   )
+
   return result.length === alphabetArray.length
 }
