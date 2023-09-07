d5aa0dd5a2c015e68e43d4a434c41e8b0bd2de62
“Lucas <lucaspereira.desenvolvedor@gmail.com>
2023-09-07 00:52:23 -0300

    adding hey bob in project

---
+++ bob/bob.ts
@@ -0,0 +1,16 @@
+const isQuestion = (message: string): boolean => message.endsWith('?')
+const isYelling = (message: string): boolean => message.toUpperCase() === message && message.toLowerCase() !== message
+const isSilence = (message: string): boolean => message.trim() === ''
+
+export function hey(message: string): string {
+
+  if (isSilence(message)) {
+    return 'Fine. Be that way!'
+  } else if (isYelling(message)) {
+    return 'Whoa, chill out!'
+  } else if (isQuestion(message)) {
+    return 'Sure.'
+  } else {
+    return 'Whatever.'
+  }
+}
