25cdb1987a1f61c0c5a5f21c03564d7bf914245f
“Lucas <lucaspereira.desenvolvedor@gmail.com>
2023-09-07 00:10:38 -0300

    implementing score method

--- darts/darts.ts
+++ darts/darts.ts
@@ -1,3 +1,11 @@
-export function score(x: unknown, y: unknown): unknown {
-  throw new Error('Remove this statement and implement this function')
+export function score(x: number, y: number): number {
+  const coordinates = Math.sqrt(x * x + y * y)
+  if (coordinates <= 1) {
+    return 10
+  } else if (coordinates <= 5) {
+    return 5
+  } else if (coordinates <= 10) {
+    return 1
+  }
+  return 0
 }
